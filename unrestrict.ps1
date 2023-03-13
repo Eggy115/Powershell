f2d8514b412c440b9a571c384ac395b612b937c5
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-13 19:36:14 +0000

    Create

---
+++ unrestrict.ps1
@@ -0,0 +1,5 @@
+set-executionpolicy -executionpolicy unrestricted -scope CurrentUser
+set-executionpolicy -executionpolicy unrestricted -scope MachinePolicy
+set-executionpolicy -executionpolicy unrestricted -scope UserPolicy 
+set-executionpolicy -executionpolicy unrestricted -scope Process
+set-executionpolicy -executionpolicy unrestricted -scope LocalMachine
