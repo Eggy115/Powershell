615372e85a7415da01cae7c2cbb325a5af6ef72c
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-20 20:10:13 +0000

    Create get parameter count.ps1

---
+++ get parameter count.ps1
@@ -0,0 +1,14 @@
+function Get-ParameterCount {
+    param (
+        [string[]]$ParameterName
+    )
+
+    foreach ($Parameter in $ParameterName) {
+        $Results = Get-Command -ParameterName $Parameter -ErrorAction SilentlyContinue
+
+        [pscustomobject]@{
+            ParameterName = $Parameter
+            NumberOfCmdlets = $Results.Count
+        }
+    }
+}
