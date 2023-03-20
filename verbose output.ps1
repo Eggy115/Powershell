82f8e38ffdfc743c3d2a14e4516be6a1146297fe
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-20 20:10:40 +0000

    Create verbose output.ps1

---
+++ verbose output.ps1
@@ -0,0 +1,14 @@
+function Test-VerboseOutput {
+
+    [CmdletBinding()]
+    param (
+        [ValidateNotNullOrEmpty()]
+        [string[]]$ComputerName = $env:COMPUTERNAME
+    )
+
+    foreach ($Computer in $ComputerName) {
+        Write-Verbose -Message "Attempting to perform some action on $Computer"
+        Write-Output $Computer
+    }
+
+}
