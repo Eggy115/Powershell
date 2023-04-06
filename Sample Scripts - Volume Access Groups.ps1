c9d841418cf8b868bbc504f2ebb6ef19435b0dc2
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-06 13:06:02 +0100

    Add files via upload

---
+++ Sample Scripts - Volume Access Groups.ps1
@@ -0,0 +1,24 @@
+﻿# Display Volume Names in Volume Access Group
+
+Get-SFVolumeAccessGroup -VolumeAccessGroupName 'name' | Get-SFVolume
+
+
+# Display InitiatorIQNs for each VolumeAccessGroup
+ $result = @()
+ $vags = Get-SFVolumeAccessGroup
+ foreach($vag in $vags){
+    $initiators = $vag.Initiators
+    foreach($init in $initiators){
+        $object = New-Object -TypeName PSObject -Property @{
+              Name = $vag.VolumeAccessGroupName
+              Initiator = $init
+        }
+        $result += $object
+    }
+ }
+ $result | Sort VolumeAccessGroupName
+
+
+# Add volume(s) to Volume Access Group
+Get-SFVolume "vol1","vol2" | Add-SFVolumeToVolumeAccessGroup -VolumeAccessGroupID 1
+
---
+++ Sample Scripts - Volumes.ps1
@@ -0,0 +1,44 @@
+﻿# Display Volume Name and Volume size in GB
+Get-SFVolume | Select Name, @{N='TotalSizeGB';E={[math]::Round($_.TotalSize/1GB*(1/0.9313),1) }}
+
+# Display Volume Name and QoS information
+Get-SFVolume | Select Name, @{N='MinIOPS';E={$_.QoS.MinIOPS}}, @{N='MaxIOPS';E={$_.QoS.MaxIOPS}}, @{N='BurstIOPS';E={$_.QoS.BurstIOPS}}
+
+# Display the Name, IQN, SCSI NAA
+Get-SFVolume | Select Name, IQN, ScsiNAADeviceID
+
+# Display Volumes for an Account or list of accounts or accountIDs
+$accounts = "Josh","Aaron"
+$accounts | Get-SFAccount | Get-SFVolume
+# or
+$accountids = 5,6,7
+Get-SFVolume -AccountID $accountids
+
+
+# Display the VolumeAccessGroups each volume is assigned
+$result = @()
+$volumes = Get-SFVolume
+foreach($vol in $volumes){
+    $volaccessgroups = $vol.VolumeAccessGroups
+    foreach($vagID in $volaccessgroups){
+        $vag = Get-SFVolumeAccessGroup $vagID
+        # Store Volume Access Group data in object
+        $object = New-Object -TypeName PSObject -Property @{
+              Name = $vol.Name
+              VolumeAccessGroupName = $vag.VolumeAccessGroupName
+        }
+        $result += $object
+    }
+ }
+ $result | Sort Name
+
+# Set Attribute
+# !!!! Note !!! Doing it this way will OVERWRITE your attributes for the volume.
+# We recommend using the Set-SFVolumeAttribute function in the SPBM module for modifying volume attributes.
+
+$attribute = @{"Workload" = "Standard"}
+Get-SFVolume Dev* | Set-SFVolume -Attributes $attribute
+
+
+ 
+ 
